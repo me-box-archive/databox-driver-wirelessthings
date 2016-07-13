14a402ae74cfcd21aea31e71065d153a1317c4a2
james <james>
2016-07-13 15:40:25 +0100

    updated

--- driver_wirelessthings/app.js
+++ driver_wirelessthings/app.js
@@ -1,12 +1,77 @@
-var mqtt    = require('mqtt');
-var serport = require('./modules/serial_node');
-var llap_helper = require('/modules/llap_helper');
+var serport;
+var lhelper = require('./modules/llap_helper');
+var mqtt = require('./modules/mqtt_publisher');
+var serialport = require('serialport');
+var exec = require('child_process').exec;
 
+exports.getPath = function()
+{
+	if(serport) {
+		return serport.path;
+	} else {
+		return "not connected";
+	}
+}
 
+var detectSerialOnMac = function()
+{
+	var port;
+	console.log('* attempting to detect a serial port on mac osx *');
+	exec('ls /dev/tty.*', function(error, stdout, stderr){
+		if (stdout){
+			console.log("*got list of ports*");
+			var ports = stdout.split('\n');
+			for (var i = ports.length - 1; i >= 0; i--){
+				console.log(ports[i]);
+				if (ports[i].search('usbmodem') != -1 || ports[i].search('usbserial') != -1 || ports[i].search('ACM0') != -1) 
+					port = ports[i];
+			}
+		}
+		if (port){
+			attemptConnection(port);
+		}	else{
+			console.log('failed to connect to serial port');
+		}
+	});
+}
 
-var client  = mqtt.connect('mqtt://localhost');
- 
-client.on('connect', function () {	
-  client.publish('presence', 'Hello mqtt');
-  client.end();
-});
+var detectSerialOnDatabox = function()
+{
+	var port;
+	console.log('* attempting to connect to a serial port on data box*');
+	serialport.list(function (e, ports) {
+		ports.forEach(function(obj) {
+			if (obj.hasOwnProperty('pnpId')){
+		// CC1111 captures the URF stick //
+				if (obj.pnpId.search('CC1111') != -1) {
+					attemptConnection(obj.comName);
+				}
+			}
+		});
+	});
+}
+
+var incomingData = "";
+var llapParser = function(emitter, buffer){
+	incomingData += buffer.toString();
+	incomingData = incomingData.replace(/^[^]*?a/,'a');
+	while (incomingData.length >= 12) {
+		emitter.emit('data',incomingData.substr(0,12));
+		incomingData = incomingData.substr(12).replace(/^[^]*?a/,'a');
+	}
+}
+
+var attemptConnection = function(port)
+{
+	console.log('* attempting to connect to serial at :', port, ' *');
+	serport = new serialport.SerialPort(port, { baudrate: 9600, parser: llapParser });
+	serport.on("open", function () {
+		console.log('* connection to a serial port successful ! *');
+		serport.on('data', function(data){
+			mqtt.onDataOverSerial(data);
+		});
+	});
+}
+
+mqtt.init();
+detectSerialOnDatabox();
--- driver_wirelessthings/modules/mqtt_publisher.js
+++ driver_wirelessthings/modules/mqtt_publisher.js
@@ -1,4 +1,3 @@
-var serport = require('./serial_node');
 var lhelper = require('./llap_helper');
 var request = require('request');
 
@@ -8,30 +7,59 @@ exports.init = function(io){
 
 exports.onDataOverSerial = function(data){
 	var payload = data.toString();
-	console.log(payload);
+	
 	if (lhelper.isValid(payload)) {
 
 		var message = lhelper.message(payload);
+		var jsonLLAP = {};
+		
+		
+		var willSend = false;
+		
 		if(message.substring(0,4) == "TMPA" || message.substring(0,4) == "TEMP") {
-			var jsonLLAP = {};
 			jsonLLAP.value = parseFloat(message.substring(4,9));
-			jsonLLAP.node = lhelper.deviceName(payload);
-			jsonLLAP.timestamp = new Date().toJSON();
 			jsonLLAP.unit = "degrees celcius";
 			jsonLLAP.stream = "temperature";
+			willSend = true;
+		}
+
+		if(message.substring(0,4) == "RHUM") {
+			jsonLLAP.value = parseFloat(message.substring(4,9));
+			jsonLLAP.unit = "%";
+			jsonLLAP.stream = "relative_humidity";
+			willSend = true;
+		}
+
+		if(message.substring(0,4) == "BATT") {
+			jsonLLAP.value = parseFloat(message.substring(4,9));
+			jsonLLAP.unit = "volts";
+			jsonLLAP.stream = "battery";
+			willSend = true;
+		}
+
+		if(willSend){
+			
+			jsonLLAP.node = lhelper.deviceName(payload);
+			jsonLLAP.timestamp = new Date().toJSON();
 			console.log(JSON.stringify(jsonLLAP));
-			request.post({
+			request.post(
+			{
 				url: 'http://localhost:8080/reading',
 				body: jsonLLAP,
 				json: true
-			}, 	function (error, response, body) {
-    				if (!error && response.statusCode == 200) {
-        				console.log(body)
-    				}
-				});
+			}, 	
+			function (error, response, body) {
+				if (!error && response.statusCode == 200) {
+	    			console.log(body)
+				}
+			});
 		}
-
+		else{
+			console.log(payload);
+		}		
 	}
+	
+
 	else {
 		console.log("message is not valid");
 		console.log(message);
